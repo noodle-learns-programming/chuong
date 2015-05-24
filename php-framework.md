1d4ce6e8b8b4698721ab938c076c0696a4d9d284
tpphu <vietean@gmail.com>
2015-05-24 15:16:41 +0700

    + Init Zend Project with Chuong

---
+++ php-framework.md
@@ -0,0 +1,8 @@
+# PHP Framework
+## Can co nhung gi?
+1. Autoload
+	Cần phải trả lời được câu hỏi, tại sao cần phải để Zend vào đúng thu mục của mình? Chưa phải là autoload
+	Để chỗ khác được không?
+	Trả lời được câu hỏi này. Nó làm em sáng tỏ về hầu hết cách load file trong mọi
+	ngôn ngữ lập trình. 
+2. Casading loading
---
+++ public/index.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * 1. Include Zend path in php
+ */
+$path = '/var/www/html/libraries';
+set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+
+
+defined('APPLICATION_PATH')
+    || define('APPLICATION_PATH',
+              realpath(dirname(__FILE__) . '/../application'));
+ 
+// Define application environment
+defined('APPLICATION_ENV')
+    || define('APPLICATION_ENV',
+              (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV')
+                                         : 'production'));
+ 
+/** Zend_Application */
+require_once 'Zend/Application.php';
+ 
+// Create application, bootstrap, and run
+$application = new Zend_Application(
+    APPLICATION_ENV,
+    APPLICATION_PATH . '/configs/application.ini'
+);
+$application->bootstrap()
+            ->run();
